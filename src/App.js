cd51ba065bbe47a594dcdb1a84bdc774fdf14401
Nita <102913196+NitaCosmin@users.noreply.github.com>
2024-03-04 15:12:52 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
