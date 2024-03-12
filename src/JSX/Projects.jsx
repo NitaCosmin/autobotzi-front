7211f370d57daca69ca72c8c95cabd851d5547c3
sebasokk <sebastian.tupa2001@gmail.com>
2024-03-12 16:02:13 +0200

    project v2

--- src/JSX/Projects.jsx
+++ src/JSX/Projects.jsx
@@ -1,13 +1,23 @@
 import Navbar from "./Navbar";
 import '../CSS/Projects.css';
+import { MdAdd } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 
 const Projects = () => {
   
     
    return(
     <div className="Container">
-    <Navbar />
-    
+        <Navbar />
+        <div className="workspaceProjects">
+
+            <div className="headerProjects">
+                <div className="namePageProject"><p className="titleProjects">Projects</p></div>
+                <div className="addProjects"><Link><MdAdd/></Link></div>
+            </div>
+        </div>
+
     </div>
 
 
